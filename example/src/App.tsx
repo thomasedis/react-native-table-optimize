32cef5789dfc602f920b9329eb855d8cfe4a18ca
thomasedis <nphuocnguyen2000@gmail.com>
2022-01-30 17:12:54 +0700

    Add README file and init base

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,19 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-table-optimize';
+import { multiply, ViewDemo } from 'react-native-table-optimize';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
   React.useEffect(() => {
-    multiply(3, 7).then(setResult);
+    multiply(3, 2).then(setResult);
   }, []);
 
   return (
     <View style={styles.container}>
       <Text>Result: {result}</Text>
+      <ViewDemo />
     </View>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,15 @@
+
+import { Text, View } from 'react-native';
+import React from 'react';
+
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
+}
+
+export function ViewDemo() {
+  return (
+    <View>
+      <Text>Hello I'm Thomas</Text>
+    </View>
+  );
 }
