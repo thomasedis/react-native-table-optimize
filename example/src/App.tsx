c052ad70b3dd821cff4f591d4a6f4645bb7f36f7
thomasedis <nphuocnguyen2000@gmail.com>
2022-01-31 01:01:54 +0700

    Add component TableWrap, TableHead and TableBody

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,28 +1,153 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply, ViewDemo } from 'react-native-table-optimize';
+import { StyleSheet, View, Text, ScrollView } from 'react-native';
+import { TableWrap, TableHead, TableBody } from 'react-native-table-optimize';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
 
-  React.useEffect(() => {
-    multiply(3, 2).then(setResult);
-  }, []);
+    let dataSpacingTHead: Array<string> = [
+        "20%",
+        "20%",
+        "20%",
+        "20%",
+        "20%",
+    ]
 
-  return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-      <ViewDemo />
-    </View>
-  );
+    let dataTitleTHead: Array<string> = [
+        "Cột 1",
+        "Cột 2",
+        "Cột 3",
+        "Cột 4",
+        "Cột 5",
+    ]
+    return (
+        <ScrollView>
+            <Text style={{textAlign: 'center', paddingVertical: 20}}>Table - Optimize</Text>
+            <TableWrap scrollHorizontal={false}>
+                <TableHead
+                    dataSpacing={dataSpacingTHead}
+                    dataTitleHead={dataTitleTHead}
+                    itemStyle={{
+                        backgroundColor: 'red',
+                        padding: 10,
+                        borderWidth: 1,
+                        borderColor: '#999'
+                    }}
+                    titleTextStyle={{
+                        textAlign: 'center',
+                    }}
+                />
+                <TableBody
+                    isScroll={true}
+                    bodyHeight={100}
+                    onRefreshTable={() => {
+                        console.log('HELLOOO');
+                        
+                    }}
+                >
+                    <View style={{flexDirection: 'row', borderBottomWidth: 1, borderBottomColor: '#999'}}>
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'red', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'red', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                    </View>
+                    <View style={{flexDirection: 'row', borderBottomWidth: 1, borderBottomColor: '#999'}}>
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'red', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'red', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                    </View>
+                    <View style={{flexDirection: 'row', borderBottomWidth: 1, borderBottomColor: '#999'}}>
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'red', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                    </View>
+                    <View style={{flexDirection: 'row', borderBottomWidth: 1, borderBottomColor: '#999'}}>
+                        <View style={{backgroundColor: 'red', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'red', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'red', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'red', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                    </View>
+                    <View style={{flexDirection: 'row', borderBottomWidth: 1, borderBottomColor: '#999'}}>
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'red', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'red', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                    </View>
+                    <View style={{flexDirection: 'row', borderBottomWidth: 1, borderBottomColor: '#999'}}>
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'red', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'red', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                    </View>
+                    <View style={{flexDirection: 'row', borderBottomWidth: 1, borderBottomColor: '#999'}}>
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'red', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'red', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                    </View>
+                    <View style={{flexDirection: 'row', borderBottomWidth: 1, borderBottomColor: '#999'}}>
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'red', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'red', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                    </View>
+                    <View style={{flexDirection: 'row', borderBottomWidth: 1, borderBottomColor: '#999'}}>
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'red', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'red', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                    </View>
+                    <View style={{flexDirection: 'row', borderBottomWidth: 1, borderBottomColor: '#999'}}>
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'red', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'red', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                    </View>
+                    <View style={{flexDirection: 'row', borderBottomWidth: 1, borderBottomColor: '#999'}}>
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'red', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'red', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                    </View>
+                    <View style={{flexDirection: 'row', borderBottomWidth: 1, borderBottomColor: '#999'}}>
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'red', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'red', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                    </View>
+                    <View style={{flexDirection: 'row', borderBottomWidth: 1, borderBottomColor: '#999'}}>
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'red', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'red', height: 50, width: '20%',}} />
+                        <View style={{backgroundColor: 'black', height: 50, width: '20%',}} />
+                    </View>
+
+                </TableBody>
+
+            </TableWrap>
+            <Text style={{textAlign: 'center', paddingVertical: 20}}>Table - Optimize</Text>
+        </ScrollView>
+    );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   box: {
     width: 60,
